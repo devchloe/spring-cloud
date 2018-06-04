29ce2692ad323ffe3b2836a8a7844e5f642eb3de
Chloe Kwon <devchloe.kwon@gmail.com>
2018-06-04 09:35:19 +0900

    create contracts for API provider (ItemController) in Groovy

---
+++ src/test/resources/contracts/itemservice/shouldReturnAllItemsWhenRequestGETMethod.groovy
@@ -0,0 +1,48 @@
+package itemservice
+import org.springframework.cloud.contract.spec.Contract
+
+Contract.make {
+    description "should return all items when GET '/' request is received"
+    request{
+        method 'GET'
+        url '/'
+        headers {
+            header('Content-Type', 'application/json;charset=UTF-8')
+        }
+    }
+    response {
+        status 200
+        body("""
+        [
+            {
+                "name": "Banana",
+                "price": 550,
+                "id": 1
+            },
+            {
+                "name": "Apple",
+                "price": 700,
+                "id": 2
+            },
+            {
+                "name": "Strawberry",
+                "price": 4000,
+                "id": 3
+            },
+            {
+                "name": "Peach",
+                "price": 5000,
+                "id": 4
+            },
+            {
+                "name": "Water mellon",
+                "price": 9000,
+                "id": 5
+            }
+        ]
+        """)
+        headers {
+            header('Content-Type': 'application/json;charset=UTF-8')
+        }
+    }
+}
