6bcd5b64db3123c7d42b511c357e7584d391d1d3
Chloe Kwon <devchloe.kwon@gmail.com>
2018-06-04 09:34:05 +0900

    create TestBase for API provider (ItemController)

---
+++ src/test/java/item/ItemServiceBase.java
@@ -0,0 +1,45 @@
+package item;
+
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import item.domain.Item;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+
+@Ignore
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ItemApp.class)
+public class ItemServiceBase {
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @Mock
+    private ItemService itemService;
+
+    @Before
+    public void setup() {
+        List<Item> items = new ArrayList<Item>();
+
+        items.add(new Item("Banana", 550.0));
+        items.add(new Item("Apple", 700.0));
+        items.add(new Item("Strawberry", 4000.0));
+        items.add(new Item("Peach", 5000.0));
+        items.add(new Item("Water mellon", 9000.0));
+
+        when(itemService.getAllItems()).thenReturn(items);
+
+        // we set up RestAssured so that the generated tests can simply use RestAssured to send requests against our controller.
+        RestAssuredMockMvc.webAppContextSetup(context);
+    }
+}
